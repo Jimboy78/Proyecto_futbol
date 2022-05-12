758550330539586c1c4db9049f373b20534df138
Jimmy <martiniseba78@gmail.com>
2022-05-12 20:51:31 -0300

    Ahora si se creo el proyecto

--- ProjectFutbol/urls.py
+++ ProjectFutbol/urls.py
@@ -1,4 +1,4 @@
-"""ProjcetFutbol URL Configuration
+"""ProjectFutbol URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
